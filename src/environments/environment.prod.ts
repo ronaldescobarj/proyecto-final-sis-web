164ee1cfcca2b7d285962d03c04f4f78748f34cd
Ronald Escobar <ronaldescobarj@gmail.com>
2019-06-19 18:32:03 -0400

    added firebase credentials to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA2hUN4bukR5KXR1dwqPtj_Oe8-5VSpU7s",
+    authDomain: "twentyonepilots.firebaseapp.com",
+    databaseURL: "https://twentyonepilots.firebaseio.com",
+    projectId: "twentyonepilots",
+    storageBucket: "twentyonepilots.appspot.com",
+    messagingSenderId: "261904215218"
+  }
 };
